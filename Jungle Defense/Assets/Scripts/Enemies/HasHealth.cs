9160ae4ee568755ae53c3c96bf295ffdf5022e63
Nathan Taylor <67647086+nathantaylor1@users.noreply.github.com>
2022-05-27 14:44:37 -0400

    Reset prefab positions to 0,0,0.

--- Jungle Defense/Assets/Scripts/Enemies/HasHealth.cs
+++ Jungle Defense/Assets/Scripts/Enemies/HasHealth.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class HasHealth : MonoBehaviour
 {
+    [Tooltip("This is the amount of damage the enemies can take before being destroyed.")]
     [SerializeField] private float health = 1f;
+    [Tooltip("This will be added to the GameManager score when this enemy is killed.")]
     [SerializeField] private float scoreAddedWhenKilled = 10f;
 
     public void TakeDamage(float amount = 1f)
