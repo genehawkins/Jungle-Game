ffb32d342c626a9399c6af570f70e3c3b0b0259d
Nathan Taylor <67647086+nathantaylor1@users.noreply.github.com>
2022-06-18 16:08:50 -0400

    Boulder no longer hits enemies multiple times.

--- Jungle Defense/Assets/Scripts/Towers & Traps/Boulder.cs
+++ Jungle Defense/Assets/Scripts/Towers & Traps/Boulder.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Boulder : MonoBehaviour
 {
-    [SerializeField] private float damage = 3f;
+    [SerializeField] private float damage = 1f;
+    private List<int> hits = new List<int>();
 
     private void OnTriggerEnter2D(Collider2D col)
     {
+        if (!col.CompareTag("Enemy")) return;
+        
+        // avoid hitting the same enemy twice:
+        int id = col.GetInstanceID(); 
+        if (hits.Contains(id)) return;
+        hits.Add(id);
+
+        // deal damage to the enemy:
         HasHealth hh = col.GetComponent<HasHealth>();
         if (hh) {
             hh.TakeDamage(damage);
