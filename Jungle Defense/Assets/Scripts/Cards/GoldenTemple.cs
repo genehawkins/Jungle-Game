93aca9665adc015ffe7666b41136ab672a28301e
tmcausla <72044880+tmcausla@users.noreply.github.com>
2022-06-20 14:20:01 -0400

    Add Golden Temple card

---
+++ Jungle Defense/Assets/Scripts/Cards/GoldenTemple.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenTemple : Card
+{
+    [SerializeField] private int goldAmount = 20;
+
+    public override void CardFunction()
+    {
+        var cardManager = GameManager.instance.cardSystem;
+
+        if (cardManager.hand.Count >= 2 && CanPlay()) {
+            PlayCard();
+
+            var randCard = cardManager.hand[Random.Range(0, cardManager.hand.Count)];
+            while (randCard == this) {
+                randCard = cardManager.hand[Random.Range(0, cardManager.hand.Count)];
+            }
+
+            cardManager.discardPile.Add(randCard);
+            cardManager.hand.Remove(randCard);
+            cardManager.availableCardSlots[handIndex] = true;  //Frees up the spot in the hand
+            randCard.gameObject.SetActive(false);
+
+            MoneyManager.AddCoins(goldAmount);
+        }
+    }
+}
