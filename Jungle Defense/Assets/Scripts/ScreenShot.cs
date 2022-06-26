4ad9cc9eae7de52f02e0bf21daf7b6f3e54e6a2e
Nathan Taylor <67647086+nathantaylor1@users.noreply.github.com>
2022-06-26 13:21:50 -0400

    Fixed Screenshot functionality

--- Jungle Defense/Assets/Scripts/ScreenShot.cs
+++ Jungle Defense/Assets/Scripts/ScreenShot.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class ScreenShot : MonoBehaviour
 {
     [SerializeField] private KeyCode key = KeyCode.F12;
-    private readonly string directoryName = "Screenshots";
-
+    public int superSize = 2;
+    
     public void TakeScreenshot()
     {
-        string datetime = DateTime.Now.ToString("dd-MM-yyyy HHmmss");
-        ScreenCapture.CaptureScreenshot("Screenshot " + datetime + ".png");
+        string datetime = DateTime.Now.ToString("dd-MM-yyyy-HHmmss");
+        ScreenCapture.CaptureScreenshot("Screenshot_" + datetime + ".png", superSize);
     }
 
     private void Update()
