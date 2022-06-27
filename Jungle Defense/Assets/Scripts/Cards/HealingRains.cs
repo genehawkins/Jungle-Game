27a3832d7ad406f6a907ac6920c800d82024776a
tmcausla <72044880+tmcausla@users.noreply.github.com>
2022-06-27 16:28:31 -0400

    Update Scene01

--- Jungle Defense/Assets/Scripts/Cards/HealingRains.cs
+++ Jungle Defense/Assets/Scripts/Cards/HealingRains.cs
@@ -20,9 +20,10 @@ public class HealingRains : Card
                     randCard = cardManager.hand[Random.Range(0, cardManager.hand.Count)];
                 }
 
+                cardManager.availableCardSlots[randCard.handIndex] = true;  //Frees up the spot in the hand
                 cardManager.discardPile.Add(randCard);
                 cardManager.hand.Remove(randCard);
-                cardManager.availableCardSlots[handIndex] = true;  //Frees up the spot in the hand
+                
                 randCard.gameObject.SetActive(false);
                 
 
