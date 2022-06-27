b61d6896198307a713d24f8950d9817764fb8069
Nathan Taylor <67647086+nathantaylor1@users.noreply.github.com>
2022-06-27 18:10:12 -0400

    Bee Hive Card Buffs

---
+++ Jungle Defense/Assets/Prefabs/Towers & Traps/Projectiles/Bees/Bee.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    [SerializeField] private float damage = 1f;
+    //private bool hasHit = false;
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (/*hasHit || */!col.CompareTag("Enemy")) return;
+        //hasHit = true; // to stop bullets from hitting multiple enemies.
+        // Debug.Log($"hit: {col.name}");
+        
+        HasHealth hh = col.GetComponent<HasHealth>();
+        if (hh)
+        {
+            // Debug.Log($"dealing {damage} damage to: {col.name}");
+            hh.TakeDamage(damage);
+        }
+        
+        Destroy(gameObject);
+    }
+}
